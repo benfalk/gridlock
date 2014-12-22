10c7d1c028264ed55d9de2c33fd36a40ddda6f5d
Benjamin Falk <benjamin.falk@yahoo.com>
2014-12-21 22:05:26 -0600

    Ride the cowboy

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Erlang rebuild of my bombvacum project
 
 I want this to be a learning project where I rebuild my classic
 multiplayer mindesweeper project of "bombvacum"
+
+### Starting the madness
+- rebar compile
+- the following in a terminal at the root of the project...
+```
+erl -pa ./ebin \
+-pa ./deps/cowboy/ebin/ \
+-pa ./deps/cowlib/ebin/ \
+-pa ./deps/ranch/ebin/
+```
+- In the erlang shell `application:start(gridlock).`
--- src/gridlock_app.erl
+++ src/gridlock_app.erl
@@ -10,6 +10,32 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    io:format("Starting Crypto....~n"),
+    ok = application:start(crypto),
+
+    io:format("Starting Ranch...~n"),
+    ok = application:start(ranch),
+
+    io:format("Starting Cowlib...~n"),
+    ok = application:start(cowlib),
+
+    io:format("Starting Cowboy...~n"),
+    ok = application:start(cowboy),
+
+    io:format("Building Route Table...~n"),
+    Dispatch = cowboy_router:compile([
+      {'_', [{'_', gridlock_web_server, []}]}
+    ]),
+
+    io:format("Starting HTTP...~n"),
+    cowboy:start_http(
+      my_gridlock_web_server,
+      10, % Number of "workers"
+      [{port, 8080}],
+      [{env, [{dispatch, Dispatch}]}]
+    ),
+
+    io:format("Starting Supervisor...~n"),
     gridlock_sup:start_link().
 
 stop(_State) ->
