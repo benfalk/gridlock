275c856ca0027e4e10d006faaa01964cd8085c5b
Benjamin Falk <bn_falk@yahoo.com>
2014-12-24 16:33:01 -0600

    Proto-grid build

--- src/gridlock_grid.erl
+++ src/gridlock_grid.erl
@@ -1,23 +1,23 @@
 -module(gridlock_grid).
 
-%-spec gridlock_grid() :: {gridlock_grid,[grid(),...]}.
-%-spec point() :: {integer(), integer()}.
-%-spec status() :: flagged | covered | uncovered.
-%-spec surrounding_bombs() :: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8.
-%-spec has_bomb() :: false | true.
-%-spec grid() :: {point(), status(), has_bomb(), surrounding_bombs()}.
+-type gridlock_grid() :: {gridlock_grid,[grid(),...]}.
+-type point() :: {integer(), integer()}.
+-type status() :: flagged | covered | uncovered.
+-type surrounding_bombs() :: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8.
+-type has_bomb() :: false | true.
+-type grid() :: {point(), status(), has_bomb(), surrounding_bombs()}.
 
-export([build/1, list_upto/1]).
+-export([build/1, list_upto/1]).
 
 build(Size) ->
   List = list_upto(Size),
-  {gridlock_grid, [{X, Y}, covered, false, 0} || X <- List, Y <- List]}.
-
+  Matrix = [{{X, Y}, covered, false, 0} || X <- List, Y <- List],
+  {gridlock_grid, Matrix}.
 
 list_upto(Size) ->
   list_upto(0, Size, []).
 
 list_upto(Size, Size, List) ->
-  lists:reverse([Size|List]);
+  lists:reverse(List);
 list_upto(Current, Max, List) ->
   list_upto(Current + 1, Max, [Current + 1 | List]).
