eb0f6b2d8458ac02af405fecaba805eb0c9649a9
Benjamin Falk <bn_falk@yahoo.com>
2015-01-11 09:32:55 -0600

    RPC, hopefully

--- src/gridlock_game.erl
+++ src/gridlock_game.erl
@@ -1,16 +1,33 @@
 -module(gridlock_game).
 
--export([start/1]).
+-export([start/1,rpc/2]).
 
 start(Size) ->
   Grid = gridlock_grid:build(Size),
   spawn(fun()-> loop(Grid) end).
+
+rpc(Game, Method, Args) ->
+  Game ! {self(), Method, Args}
+  receive
+    {Game, Msg} -> Msg;
+    Unkown      -> io:format("Unkown msg [~p]~n", [Unkown])
+  end.
+
+rpc(Game, Method) ->
+  Game ! {self(), Method}
+  receive
+    {Game, Msg} -> Msg;
+    Unkown      -> io:format("Unkown msg [~p]~n", [Unkown])
+  end.
 
 loop(Grid) ->
   receive
     {From, uncover, {X,Y}} -> Grid2 = Grid:update({X,Y}, uncovered),
                               From ! {self(), ok},
                               loop(Grid2);
+
+    {From, grid}           -> From ! {self(), Grid},
+                              loop(Grid);
 
     Unkown                 -> io:format("Received unkown [~p]~n", [Unkown])
   end.
