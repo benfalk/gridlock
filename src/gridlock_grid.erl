a29ad9f2aa5fd6c066a6d314133f8c05b3e9a3b4
Benjamin Falk <bn_falk@yahoo.com>
2015-02-02 22:55:31 -0600

    Man I love when tests drive

--- src/gridlock_grid.erl
+++ src/gridlock_grid.erl
@@ -1,6 +1,24 @@
 -module(gridlock_grid).
 
--export([build/1]).
+-export([
+  build/1,
+  square_at/2,
+  surrounding_locations/2
+]).
 
 build(Size) ->
-  #{size => Size, grid => #{}}.
+  DefaultSquare = #{has_bomb => false, status => covered, surrounding_bombs => 0},
+  ProtoMap = [{{X,Y}, DefaultSquare} || X <- lists:seq(1,Size), Y <- lists:seq(1,Size)], 
+  #{size => Size, grid => maps:from_list(ProtoMap)}.
+
+square_at(#{grid := Grid}, Location) when is_map(Grid), is_tuple(Location) ->
+  maps:get(Location, Grid).
+
+surrounding_locations(#{size := Size, grid := Grid}, {X,Y}) ->
+  Keys = [{X2,Y2} || X2 <- lists:seq(X-1,X+1),
+                     Y2 <- lists:seq(Y-1,Y+1),
+                     X2 > 0,
+                     X2 =< Size,
+                     Y2 > 0,
+                     Y2 =< Size] -- [{X,Y}],
+  Reduced = maps:with(Keys, Grid).
--- test/gridlock_grid_tests.erl
+++ test/gridlock_grid_tests.erl
@@ -5,3 +5,24 @@ build_test() ->
   Grid = gridlock_grid:build(5),
   ?assertMatch(#{ size := 5}, Grid),
   ?assertMatch(#{ grid := G} when is_map(G), Grid).
+
+square_at_test() ->
+  Grid = gridlock_grid:build(5),
+  Square = gridlock_grid:square_at(Grid, {1,1}),
+  ?assertMatch(#{ has_bomb := _, status :=_, surrounding_bombs :=_}, Square).
+
+surrounding_locations_test() ->
+  Grid = gridlock_grid:build(5),
+  Locations1x1 = gridlock_grid:surrounding_locations(Grid, {1,1}),
+  ?assertMatch(X when is_map(X), Locations1x1),
+  ?assertMatch(3, maps:size(Locations1x1)),
+  ?assertMatch(#{{1,2} := _}, Locations1x1),
+  ?assertMatch(#{{2,1} := _}, Locations1x1),
+  ?assertMatch(#{{2,2} := _}, Locations1x1),
+
+  Locations5x5 = gridlock_grid:surrounding_locations(Grid, {5,5}),
+  ?assertMatch(X when is_map(X), Locations5x5),
+  ?assertMatch(3, maps:size(Locations5x5)),
+  ?assertMatch(#{{4,5} := _}, Locations5x5),
+  ?assertMatch(#{{4,4} := _}, Locations5x5),
+  ?assertMatch(#{{5,4} := _}, Locations5x5).
