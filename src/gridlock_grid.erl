f9df02c0547cfbd6339a5da4d0a95bb5d0760bb0
Benjamin Falk <bn_falk@yahoo.com>
2015-02-03 19:24:45 -0600

    Moar gridlock refactoring

--- src/gridlock_grid.erl
+++ src/gridlock_grid.erl
@@ -3,22 +3,33 @@
 -export([
   build/1,
   square_at/2,
-  surrounding_locations/2
+  surrounding_locations/2,
+  plant_bombs/2
 ]).
 
 build(Size) ->
   DefaultSquare = #{has_bomb => false, status => covered, surrounding_bombs => 0},
   ProtoMap = [{{X,Y}, DefaultSquare} || X <- lists:seq(1,Size), Y <- lists:seq(1,Size)], 
-  #{size => Size, grid => maps:from_list(ProtoMap)}.
+  #{size => Size, squares => maps:from_list(ProtoMap)}.
 
-square_at(#{grid := Grid}, Location) when is_map(Grid), is_tuple(Location) ->
+square_at(#{squares := Grid}, Location) when is_map(Grid), is_tuple(Location) ->
   maps:get(Location, Grid).
 
-surrounding_locations(#{size := Size, grid := Grid}, {X,Y}) ->
+surrounding_locations(#{size := Size, squares := Grid}, {X,Y}) ->
   Keys = [{X2,Y2} || X2 <- lists:seq(X-1,X+1),
                      Y2 <- lists:seq(Y-1,Y+1),
                      X2 > 0,
                      X2 =< Size,
                      Y2 > 0,
                      Y2 =< Size] -- [{X,Y}],
-  Reduced = maps:with(Keys, Grid).
+  maps:with(Keys, Grid).
+
+plant_bombs(Grid = #{size := Size, squares := Squares}, BombAmount) ->
+  BombList = lists:duplicate(BombAmount, true) ++ lists:duplicate(Size*Size-BombAmount, false),
+  SquaresList = maps:to_list(Squares),
+  Seeded = plant_bombs(BombList, SquaresList, []),
+  Grid#{squares := maps:from_list(Seeded)}.
+
+plant_bombs([],[],List) -> lists:reverse(List);
+plant_bombs([Bomb|BombList], [{Cord,Square}|SquaresList], Acc) ->
+  plant_bombs(BombList, SquaresList, [{Cord,Square#{has_bomb := Bomb}}|Acc]).
--- test/gridlock_grid_tests.erl
+++ test/gridlock_grid_tests.erl
@@ -4,7 +4,7 @@
 build_test() ->
   Grid = gridlock_grid:build(5),
   ?assertMatch(#{ size := 5}, Grid),
-  ?assertMatch(#{ grid := G} when is_map(G), Grid).
+  ?assertMatch(#{ squares := G} when is_map(G), Grid).
 
 square_at_test() ->
   Grid = gridlock_grid:build(5),
@@ -25,4 +25,25 @@ surrounding_locations_test() ->
   ?assertMatch(3, maps:size(Locations5x5)),
   ?assertMatch(#{{4,5} := _}, Locations5x5),
   ?assertMatch(#{{4,4} := _}, Locations5x5),
-  ?assertMatch(#{{5,4} := _}, Locations5x5).
+  ?assertMatch(#{{5,4} := _}, Locations5x5),
+
+  Locations2x2 = gridlock_grid:surrounding_locations(Grid, {2,2}),
+  ?assertMatch(X when is_map(X), Locations2x2),
+  ?assertMatch(8, maps:size(Locations2x2)),
+  ?assertMatch(#{{1,1} := _}, Locations2x2),
+  ?assertMatch(#{{1,2} := _}, Locations2x2),
+  ?assertMatch(#{{1,3} := _}, Locations2x2),
+  ?assertMatch(#{{2,1} := _}, Locations2x2),
+  ?assertMatch(#{{2,3} := _}, Locations2x2),
+  ?assertMatch(#{{3,1} := _}, Locations2x2),
+  ?assertMatch(#{{3,2} := _}, Locations2x2),
+  ?assertMatch(#{{3,3} := _}, Locations2x2).
+
+plant_bombs_test() ->
+  Grid = gridlock_grid:build(5),
+  #{size:= 5, squares := Squares} = gridlock_grid:plant_bombs(Grid, 5),
+  Bombs = maps:fold(fun(_,#{has_bomb := B}, Ammount) -> case B of
+                                                          true -> Ammount + 1;
+                                                          false -> Ammount
+                                                        end end, 0, Squares),
+  ?assertMatch(5, Bombs).
